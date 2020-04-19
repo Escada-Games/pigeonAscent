b6f5efc7e7eb355da0239fb753f6dfcd18dd7706
Delta-key <keybpr@gmail.com>
2020-04-19 19:45:31 -0300

    Implemented enemy generation all the way to lv10

--- scenes/global.gd
+++ scenes/global.gd
@@ -43,11 +43,11 @@ func calculateFood(strength,speed):
 func updatePlayerHp():
 	var oldMaxHp=self.player.maxHp
 	self.player.maxHp=calculateHp(self.player.strength,self.player.defense)
-	self.player.hp=(self.player.hp/oldMaxHp)*self.player.maxHp
+	self.player.hp=int((1.0*self.player.hp/oldMaxHp)*1.0*self.player.maxHp)
 func updatePlayerFood():
 	var oldMaxEnergy=self.player.maxEnergy
 	self.player.maxEnergy=calculateFood(self.player.strength,self.player.speed)
-	self.player.energy=self.player.energy*self.player.maxEnergy/oldMaxEnergy
+	self.player.energy=int(1.0*self.player.energy*self.player.maxEnergy/oldMaxEnergy)
 func updatePlayerHpAndFood():
 	self.updatePlayerHp()
 	self.updatePlayerFood()
@@ -57,7 +57,6 @@ var opponents=preload("res://scenes/opponentPanel.tscn")
 var battleScene=preload("res://scenes/battleScene.tscn")
 func _ready():set_process(true)
 func _process(delta):
-	print(String(self.player.hp)+"/"+String(self.player.maxHp))
 	OS.set_window_title("Pigeon Ascent -- " + String(Engine.get_frames_per_second()) + "FPS")
 func fight():
 	get_tree().root.add_child(opponents.instance())
