70926f0676ac1af5c7d23db2a02cb96c8a5735cb
Delta-key <keybpr@gmail.com>
2020-07-18 19:04:40 -0300

    Enemies should be reset properly after retrying

--- scenes/battleSceneNew.gd
+++ scenes/battleSceneNew.gd
@@ -254,6 +254,8 @@ func killMe(h,m):
 func gameOver():
 	#TODO:CHANGE TO GOING BACK TO THE TITLE SCREEN
 #	get_tree().quit()
+#	get_tree().change_scene("res://scenes/intro.tscn")
+	global.level=1
 	get_tree().change_scene("res://scenes/intro.tscn")
 	self.queue_free()
 func effects(area):
